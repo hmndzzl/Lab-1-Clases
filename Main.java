b117a1710b2b6b334dfd780db851ee9e316476a7
hmndzzz <hrmendezlee@gmail.com>
2024-08-02 13:00:05 -0600

    avance en la logica del juego

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import java.util.Random;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int max = 21;
 
         System.out.println("Ingrese su nombre: ");
         String nombre = scanner.nextLine();
@@ -18,17 +20,41 @@ public class Main{
             int opcion = scanner.nextInt();
             switch (opcion)  {
                 case 1:
-                    draw
+                    player.setMano(random.nextInt(10) + random.nextInt(10));
+                    dealer.setMano(random.nextInt(10) + random.nextInt(10));
+                    if (player.getMano() <= max && dealer.getMano() <+ max){
+                        int seguir2 = 1;
+                        while (seguir2 == 1){
+                            System.out.println(nombre + " tu mano es: " + player.getMano());
+                            System.out.println("¿Quieres pedir carta? Si : 1 | No:  2");
+                            int decision = scanner.nextInt();
+                            switch (decision)  {
+                            case 1:
+                                player.drawCard();
+                                System.out.println("Tu mano es: " + player.getMano());
+                                if (player.getMano() > max){
+                                    System.out.println("Te has pasado de 21!");
+                                    seguir = 0;
+                                }
+                                break;
+                            case 2:
+                                seguir2 = 0;
+                                break;
+                            default:
+                                System.out.println("Error, ingrese una opción válida");
+                        }
+                        
+                    }
                     break;
+
                 case 2:
-                    System.err.println("Terminando juego...");
+                    System.out.println("Terminando juego...");
                     seguir = 0;
                     break;
                 default:
                     System.out.println("Error, por favor ingrese una opción válida.");
             }
         }
-    
         
     }
 }
